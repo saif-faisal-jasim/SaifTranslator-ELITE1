10c29c34002c720fe36c89402352b9dbecbd1a5c
saif-faisal-jasim <saiffa36@gmail.com>
2026-04-05 13:43:30 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/saiftranslator/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.saiftranslator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Text("Welcome to Saif Translator ELITE")
+}
